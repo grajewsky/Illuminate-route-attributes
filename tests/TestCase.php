6066ee2669f39cfffdab5fb4ef1845836c179893
Lukasz Grajewski <grajewski.lukasz@wp.pl>
2021-06-12 12:45:39 +0200

    Update testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace GSSC\Routes\Tests;
 
-class TestCase extends \PHPUnit\Framework\TestCase
+use GSSC\Routes\Providers\ServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
 {
 
+    protected function getPackageProviders($app): array
+    {
+         return [
+             ServiceProvider::class
+         ];
+    }
 }
